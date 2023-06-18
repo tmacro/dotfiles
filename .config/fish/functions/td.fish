68c6f35ae922bd923bc1198d7f3a99f5bfbbbff4
Taylor McKinnon <mail@tmacs.space>
2023-06-18 15:19:55 -0700

    add td wrapper changes

--- .config/fish/functions/td.fish
+++ .config/fish/functions/td.fish
@@ -1,12 +1,12 @@
 function td --description 'Daily scratch directory manager'
     if test (count $argv) -eq 0
-        cd (command today dir show)
-        return
-    end
-    if test (count $argv) -eq 1 && string match --quiet --regex '^\-?\d+' -- "$argv";
-        cd (command today dir show $argv)
+        cd (command td show)
         return
     end
 
-    command today dir show $argv
+    if test (count $argv) -eq 1 && string match --quiet --regex '^\-?\d+' -- "$argv";
+        cd (command td show $argv)
+        return
+    end
+    command td $argv    
 end
