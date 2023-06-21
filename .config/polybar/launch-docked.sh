49cce0a333ad50d7a88bfd7115d11083d8a56055
Taylor McKinnon <mail@tmacs.space>
2023-06-20 20:06:43 -0700

    cleanup monitors

--- .config/polybar/launch-docked.sh
+++ .config/polybar/launch-docked.sh
@@ -6,10 +6,10 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-MONITOR="$MON_PRIMARY_DOCKED" polybar primary-docked -c ~/.config/polybar/config.ini &
+MONITOR="$MON_PRIMARY_DOCKED" polybar primary -c ~/.config/polybar/config.ini &
 
 if [ -n "$MON_SECONDARY_DOCKED" ]; then
-    MONITOR="$MON_SECONDARY_DOCKED" polybar -c ~/.config/polybar/config.ini secondary-docked &
+    MONITOR="$MON_SECONDARY_DOCKED" polybar -c ~/.config/polybar/config.ini secondary &
 fi
 
 if [ -n "$MON_BUILTIN" ]; then
