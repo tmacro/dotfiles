ac0771fa5d66f0d853e5ae1e66474d2ceb89e64c
Taylor McKinnon <mail@tmacs.space>
2024-07-11 21:13:58 -0700

    fix negative relative numbers for td

--- .config/fish/functions/td.fish
+++ .config/fish/functions/td.fish
@@ -5,7 +5,7 @@ function td --description 'Daily scratch directory manager'
     end
 
     if test (count $argv) -eq 1 && string match --quiet --regex '^\-?\d+' -- "$argv";
-        cd (command td show $argv)
+        cd (command td show -- $argv)
         return
     end
     command td $argv    
