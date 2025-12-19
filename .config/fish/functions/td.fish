643b31d6c4bfd98366ec5a2319fd53c37d3130e5
Taylor McKinnon <mail@tmacs.space>
2025-12-18 18:22:25 -0800

    Create scratch directory before trying the change to it. Fix background command ordering.

--- .config/fish/functions/td.fish
+++ .config/fish/functions/td.fish
@@ -1,12 +1,16 @@
 function td --description 'Daily scratch directory manager'
     if test (count $argv) -eq 0
-        cd (command td show)
+        set target_dir (command td show)
+        mkdir -p "$target_dir"
+        cd "$target_dir"
         return
     end
 
     if test (count $argv) -eq 1 && string match --quiet --regex '^\-?\d+' -- "$argv";
-        cd (command td show -- $argv)
+        set target_dir (command td show -- $argv)
+        mkdir -p "$target_dir"
+        cd "$target_dir"
         return
     end
-    command td $argv    
+    command td $argv
 end
